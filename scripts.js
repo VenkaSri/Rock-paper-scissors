a862d7172d1dfa04ddf8291403bf47de5d5d8c01
Venka <venkasritharan@gmail.com>
2021-11-11 22:22:28 -0500

    added multiple functions

--- scripts.js
+++ scripts.js
@@ -1,4 +1,3 @@
-console.log("Hello, world!");
 
 function computerPlay() {
     let randomNumber = Math.floor(Math.random() * 3);
@@ -15,5 +14,38 @@ function computerPlay() {
             break;
     }
 
-    console.log(randomNumber);
+    return randomNumber;
 }
+
+const playerSelection = prompt("Enter your choice");
+const computerSelection = computerPlay();
+
+console.log(computerSelection);
+
+function playRound(playerSelection, computerSelection) {
+
+    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+    
+    if (playerSelection == "Rock" && computerSelection == "Paper") {
+        return console.log("You lose! Paper beats rock");    
+    } else if (playerSelection == "Paper" && computerSelection == "Scissors") {
+        return console.log("You lose! Scissors beats paper");    
+    } else if (playerSelection == "Scissors" && computerSelection == "Rock") {
+        return console.log("You lose! Rock beats scissors");    
+    } else if (playerSelection == "Rock" && computerSelection == "Scissors") {
+        return console.log("You win! Rock beats scissors");    
+    } else if (playerSelection == "Paper" && computerSelection == "Rock") {
+        return console.log("You win! Paper beats Rock");    
+    } else if (playerSelection == "Scissors" && computerSelection == "Paper") {
+        return console.log("You win! Scissors beats paper");    
+    }else { 
+        return console.log("It's a tie");
+    } 
+}
+
+function game() {
+    playRound(playerSelection, computerSelection);
+}
+
+game();
+
