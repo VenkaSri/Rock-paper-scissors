3465626b96e27bbf3e1e30e139b404e2cac099a2
Venka <venkasritharan@gmail.com>
2021-12-03 21:32:29 -0500

    Added changes from rps-ui branch

--- script.js
+++ script.js
@@ -7,18 +7,11 @@ const rockButton = document.querySelector(".rock");
 const paperButton = document.querySelector(".paper");
 const scissorsButton = document.querySelector(".scissors");
 const startButton = document.querySelector('.start button');
-<<<<<<< HEAD
-
-
-
-  function computerPlay() {
-=======
 const scoreboard = document.querySelector('.scoreboard');
 const displayPlayerCount = document.querySelector('.playerScore');
 const displayComputerCount = document.querySelector('.computerScore');
 
 function computerPlay() {
->>>>>>> rps-ui
     let randomNumber = Math.floor(Math.random() * 3);
 
     switch (randomNumber) {
@@ -38,32 +31,6 @@ function computerPlay() {
     return randomNumber;
 }
 
-<<<<<<< HEAD
-  function playRound(playerSelection, computerSelection) {
-
-    
-    
-    if (playerSelection == "Rock" && computerSelection == "Paper") {
-        computerScore++;
-        return console.log("You lose! Paper beats rock");    
-    } else if (playerSelection == "Paper" && computerSelection == "Scissors") {
-        computerScore++;
-        return console.log("You lose! Scissors beats paper");    
-    } else if (playerSelection == "Scissors" && computerSelection == "Rock") {
-        computerScore++;
-        return console.log("You lose! Rock beats scissors");    
-    } else if (playerSelection == "Rock" && computerSelection == "Scissors") {
-        yourScore++;
-        return console.log("You win! Rock beats scissors");    
-    } else if (playerSelection == "Paper" && computerSelection == "Rock") {
-        yourScore++;
-        return console.log("You win! Paper beats Rock");    
-    } else if (playerSelection == "Scissors" && computerSelection == "Paper") {
-        yourScore++;
-        return console.log("You win! Scissors beats paper");    
-    }else { 
-        return console.log("It's a tie");
-=======
 function playRound(playerSelection, computerSelection) {
 
     if (playerSelection == "Rock" && computerSelection == "Paper") {
@@ -84,7 +51,6 @@ function playRound(playerSelection, computerSelection) {
         displayPlayerCount.textContent = ++yourScore;  
     }else { 
         return ;
->>>>>>> rps-ui
     } 
 }
 
@@ -94,48 +60,21 @@ function removeClass() {
     paperButton.classList.remove("addclass");
 }
 
-<<<<<<< HEAD
-
-rockButton.addEventListener("click", function() {
-    const playerSelection = "Rock";
-    const displayRound = document.querySelector('.card-round-count');
-
-    displayRound.textContent = roundCount++;
-    removeClass();
-    playRound(playerSelection, computerPlay());
-    
-    
-=======
 rockButton.addEventListener("click", function() {
     const playerSelection = "Rock";
 
     removeClass();
     playRound(playerSelection, computerPlay());     
->>>>>>> rps-ui
 });
 
 paperButton.addEventListener("click", function() {
     const playerSelection = "Paper";
-<<<<<<< HEAD
-    const displayRound = document.querySelector('.card-round-count');
-
-    displayRound.textContent = roundCount++;
-=======
-
->>>>>>> rps-ui
     removeClass();
     playRound(playerSelection, computerPlay());
 });
 
 scissorsButton.addEventListener("click", function() {
     const playerSelection = "Scissors";
-<<<<<<< HEAD
-    const displayRound = document.querySelector('.card-round-count');
-
-    displayRound.textContent = roundCount++;
-=======
-
->>>>>>> rps-ui
     removeClass();
     playRound(playerSelection, computerPlay());
 });
@@ -146,17 +85,9 @@ startButton.addEventListener('click', function() {
 });
 
 function showElements() {
-<<<<<<< HEAD
-    document.querySelector('.card-title').classList.remove('hide');
-    document.querySelector('.card-round-count').classList.remove('hide');
-    rockButton.classList.remove('hide');
-    scissorsButton.classList.remove('hide');
-    paperButton.classList.remove('hide');
-=======
     rockButton.classList.remove('hide');
     scissorsButton.classList.remove('hide');
     paperButton.classList.remove('hide');
     scoreboard.classList.remove('hide');
->>>>>>> rps-ui
 }
 
